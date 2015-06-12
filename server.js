f7fb49d1ea2cba17b0ecfc000447e8c435cf8543
Sam Mateosian <sam@bigroomstudios.com>
2015-06-12 16:30:44 -0400

    added browser sync

--- server.js
+++ server.js
@@ -10,47 +10,94 @@ var config = require('./server/config');
 var server = new Hapi.Server();
 
 server.connection({
-    host: config.host,
-    port: config.port,
-    routes: config.hapi.options.routes
+  host: config.host,
+  port: config.port,
+  routes: config.hapi.options.routes
 });
 
 var registerOpts = [
-    {
-    	register: require("./index")
-    }
+  {
+  	register: require("./index")
+  }
 ];
 
+
+var browserSync;
+
+if(config.env == 'development'){
+
+  browserSync = require('browser-sync').create();
+
+  //restart the server if js files change
+  //does not work - it needs to reload at the node level duh
+
+  //browserSync.watch("server/**/*.js").on("change", function(){
+  //  server.stop();
+  //  server.start(browserSync.reload);
+  //});
+}
+
+var startSync = function(){
+
+  //use browserSync in dev mode
+  if(config.env == 'development' && !browserSync.active){
+
+    browserSync.init({
+
+      proxy: "localhost:3000",
+      files: [
+        'public/**/*.{js,css,html}',
+        'server/views/*.html'
+      ]
+
+    });
+  }
+}
+
+var start = function(cb){
+
+  // Add good logging
+  registerOpts.push({
+    register: require("good"),
+    options: {
+      reporters: [{
+    		reporter: require('good-console'),
+    		args: [{
+    		    request: '*',
+    		    log: '*',
+    		    error: '*',
+    		    ops: '*'
+    		}]
+      }]
+    }
+  });
+
+  server.register(registerOpts, function(err) {
+
+    if (err) throw err;
+
+    server.start(function() {
+
+      console.log("Hapi server started @ " + server.info.uri.replace('0.0.0.0', 'localhost'));
+
+      if(cb) {
+
+        cb();
+      }
+
+    });
+
+  });
+
+}
+
 module.exports = {
-    server: server,
-    registerOpts: registerOpts
+  server: server,
+  registerOpts: registerOpts,
+  start: start
 }
 
 if (!module.parent) {
 
-    // Add good
-    registerOpts.push({
-        register: require("good"),
-        options: {
-            reporters: [{
-		reporter: require('good-console'),
-		args: [{
-		    request: '*',
-		    log: '*',
-		    error: '*',
-		    ops: '*'
-		}]
-            }]
-        }
-    });
-
-    server.register(registerOpts, function(err) {
-
-	    if (err) throw err;
-
-	    server.start(function() {
-		console.log("Hapi server started @ " + server.info.uri.replace('0.0.0.0', 'localhost'));
-	    });
-    });
-
+  start(startSync);
 }
