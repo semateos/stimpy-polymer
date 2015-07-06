b93cc9fede5a7273398424ecd98f003b880908b7
Sam Mateosian <sam@bigroomstudios.com>
2015-07-06 16:16:12 -0400

    routing works

--- public/js/app.js
+++ public/js/app.js
@@ -15,32 +15,15 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
-  };
+  //app.displayInstalledToast = function() {
+  //  document.querySelector('#caching-complete').show();
+  //};
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
     console.log('Our app is ready to rock!');
   });
-
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    document.querySelector('body').removeAttribute('unresolved');
-
-    // Ensure the drawer is hidden on desktop/tablet
-    //var drawerPanel = document.querySelector('#paperDrawerPanel');
-    //drawerPanel.forceNarrow = true;
-  });
-
-  // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
-    //var drawerPanel = document.querySelector('#paperDrawerPanel');
-    //if (drawerPanel.narrow) {
-    //  drawerPanel.closeDrawer();
-    //}
-  };
 
 })(document);
 
